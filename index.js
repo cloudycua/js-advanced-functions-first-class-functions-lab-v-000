ec58ac2cbc5a4840c57085e116d027ee759df11b
Christine Cua <cloudycua@gmail.com>
2019-02-13 21:41:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const returnFirstTwoDrivers = function(drivers) {
     return drivers.slice(0, 2);
 };
 
-const returnLastTwoDrivers = function() {
-
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(0, -2);
 };
